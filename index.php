ebb419d97ebd451afdf4bb7c95eafa88a971320f
Pranavkant Nigam <nigamkpranav@gmail.com>
2021-05-02 12:39:13 +0530

    Starting The Project

---
+++ index.php
@@ -0,0 +1,72 @@
+<?php
+    session_start();
+    
+?>
+<html>
+
+<head>
+    <title>Online Classroom</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link href="https://fonts.googleapis.com/css2?family=Righteous&display=swap" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+    <link rel="stylesheet" href="style/style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <style>
+        .container{
+            border:3px ridge green; 
+        }
+       
+        @media screen and (max-width: 550px) {
+            .container{
+                border: none;
+            }
+        }
+    </style>
+</head>
+
+<body onexit="<?php session_abort();?>">
+    <div class="jumbotron bg-success"
+        style="text-align:center; padding: 25px; font-size: 4vw;color:white;font-family:Righteous">Online Classroom
+    </div>
+    <br />
+    <div class="container" style="text-align:center; justify-content:center; display:flex;">
+        <div class="row" style="padding:30px; margin-top:20px; margin-bottom:20px;">
+            <div class="col-md-6">
+                <div class="card" style="width: 18rem;">
+                    <img class="card-img-top" src="images/teacher.png" alt="Teacher Logo" width="200px" height="200px;"
+                        style="padding:15px;">
+                        <hr/>
+                    <div class="card-body bg-success" style="margin-top:-10px;">
+                        <a href="teacher/teachers.php" class="text-primary"  style="text-decoration:none; font-weight:bold; font-size:large;font-family:Poppins; color:limegreen; margin-top:-10px;">
+                            <p class="card-text !text-primary linkStyle" style="text-align:center; color:white;">Teachers</p>
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6">
+                <div class="card" style="width: 18rem;">
+                    <img class="card-img-top" src="images/student.png" alt="Student Logo" width="200px" height="200px;"
+                        style="padding:15px;">
+                        <hr/>
+                    <div class="card-body bg-success" style="margin-top:-10px;">
+                        <a href="student/students.php" class="text-primary" style="text-decoration:none; font-weight:bold; font-size:large;font-family:Poppins; color:limegreen; margin-top:-10px;">
+                            <p class="card-text !text-primary linkStyle" style="text-align:center; color:white;">Students</p>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</body>
+
+</html>
---
+++ student/students.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]>      <html class="no-js"> <!--<![endif]-->
+<html>
+    <head>
+    <title>Students | Online Classroom</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link href="https://fonts.googleapis.com/css2?family=Righteous&display=swap" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <style>
+        .container{
+            border:3px ridge green; 
+        }
+       
+        @media screen and (max-width: 550px) {
+            .container{
+                border: none;
+            }
+        }
+    </style>
+    </head>
+    <body>
+    <div class="jumbotron bg-success"
+        style="text-align:center; padding: 25px; font-size: 4vw;color:white;font-family:Righteous">Students Login and Registration
+    </div>
+    <br/>
+    <div class="container" style="text-align:center; justify-content:center; display:flex;">
+        <div class="row" style="padding:30px; margin-top:20px; margin-bottom:20px;">
+            <div class="col-md-6">
+                <div class="card" style="width: 18rem;text-align:center;">
+                <i class="fa fa-sign-in" aria-hidden="true" style="width:200px; height: 200px; padding:15px; font-size:200px;text-align:center;justify-content:center;margin: 0 auto; color:brown;"></i>
+                        <hr/>
+                    <div class="card-body bg-success" style="margin-top:-10px;">
+                        <a href="students-login.php" class="text-primary"  style="text-decoration:none; font-weight:bold; font-size:large;font-family:Poppins; color:limegreen; margin-top:-10px;">
+                            <p class="card-text !text-primary linkStyle" style="text-align:center; color:white;">Login</p>
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6">
+                <div class="card" style="width: 18rem;">
+                <i class="fa fa-plus" aria-hidden="true" style="width:200px; height: 200px; padding:15px; font-size:200px;text-align:center;justify-content:center;margin: 0 auto;color:brown;"></i>
+
+                        <hr/>
+                    <div class="card-body bg-success" style="margin-top:-10px;">
+                        <a href="students-signup.php" class="text-primary" style="text-decoration:none; font-weight:bold; font-size:large;font-family:Poppins; color:limegreen; margin-top:-10px;">
+                            <p class="card-text !text-primary linkStyle" style="text-align:center; color:white;">Sign Up</p>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+        
+        <script src="" async defer></script>
+    </body>
+</html>
