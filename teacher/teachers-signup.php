9c0f601681f23c8aba8a2923e853ae47689e6854
PINKY <pinkypd09@gmail.com>
2021-05-06 12:25:46 -0700

    teacher registration

--- teacher/teachers-signup.php
+++ teacher/teachers-signup.php
@@ -54,40 +54,13 @@ form {
 .firstname {
     position: relative;
     left: 200px;
-    top: -37px;
+    top: -40px;
     line-height: 40px;
-    border-radius: 6px;
-    padding: 0 22px;
-    font-size: 16px;
-}
-
-.lastname {
-    position: relative;
-    left: 417px;
-    top: -80px;
-    line-height: 40px;
+    width: 480px;
     border-radius: 6px;
     padding: 0 22px;
     font-size: 16px;
     color: #555;
-}
-
-.firstlabel {
-    position: relative;
-    color: #E5E5E5;
-    text-transform: capitalize;
-    font-size: 14px;
-    left: 203px;
-    top: -45px;
-}
-
-.lastlabel {
-    position: relative;
-    color: #E5E5E5;
-    text-transform: capitalize;
-    font-size: 14px;
-    left: 175px;
-    top: -45px;
 }
 
 .username {
@@ -126,83 +99,6 @@ form {
     color: #555;
 }
 
-.conpassword {
-    position: relative;
-    left: 200px;
-    top: -37px;
-    line-height: 40px;
-    width: 480px;
-    border-radius: 6px;
-    padding: 0 22px;
-    font-size: 16px;
-    color: #555;
-}
-
-.code {
-    position: relative;
-    left: 200px;
-    top: -37px;
-    line-height: 40px;
-    width: 95px;
-    border-radius: 6px;
-    padding: 0 22px;
-    font-size: 16px;
-    color: #555;
-}
-
-.number {
-    position: relative;
-    left: 200px;
-    top: -37px;
-    line-height: 40px;
-    width: 255px;
-    border-radius: 6px;
-    padding: 0 22px;
-    font-size: 16px;
-    color: #555;
-}
-
-.area_code {
-    position: relative;
-    color: #E5E5E5;
-    text-transform: capitalize;
-    font-size: 16px;
-    left: 54px;
-    top: -4px;
-}
-
-.phone-number {
-    position: relative;
-    color: #E5E5E5;
-    text-transform: capitalize;
-    font-size: 16px;
-    left: -100px;
-    top: -2px;
-}
-
-#coustomer {
-    margin-left: 25px;
-    color: white;
-    font-size: 18px;
-}
-
-.radio {
-    display: inline-block;
-    padding-right: 70px;
-    font-size: 15px;
-    margin-left: 25px;
-    margin-top: 15px;
-    color: white;
-}
-
-.radio input {
-    width: 20px;
-    height: 20px;
-    border-radius: 50%;
-    cursor: pointer;
-    outline: none;
-}
-
 button {
     background-color: #3BAF9F;
     display: block;
@@ -230,13 +126,11 @@ button:hover {
 <body>
 	<div class="regform"><h1> TEACHER REGISTRATION </h1></div>
 	<div class="main">
-		<form action="connect.php" method="post">
+		<form id="main" method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
 			<div id="name">
 				<h2 class="name">NAME</h2>
 				<input class="firstname" type="text" name="first_name"><br>
-				<label class="firstlabel">FIRST NAME</label>
-				<input class="lastname" type="text" name="last_name">
-				<label class="lastlabel">LAST NAME</label>
+				
 			</div>
 
 			<h2 class="name">Teacher-ID</h2>
@@ -248,33 +142,42 @@ button:hover {
 			<h2 class="name">Password</h2>
 			<input class="password" type="text" name="password">
 
-			<h2 class="name">*Conform-Password</h2>
-			<input class="conpassword" type="text" name="conpassword">
+			
 
-			<h2 class="name">Phone Number</h2>
-			<input class="code" type="text" name="area_code">
-			<lable class="area_code">Area Code</lable>
-			<input class="number" type="text" name="phone">
-			<label class="phone-number">Phone Number</label>
 
-			<h2 id="coustomer">GENDER</h2>
-
-			<label class="radio">
-				<input class="radio-one" type="radio" checked="checked" name="">
-				<span class="checkmark"></span>
-				Male
-				</label>
-				<label class="radio">
-					<input class="radio-two" type="radio" checked="checked" name="">
-					<span class="checkmark"></span>
-					Female
-				</label>
-
-					<button type="submit">Register</button>
+					<button type="submit" name="submit">Register</button>
 
 
 			
 		</form>
+
+        <?php 
+             if(isset($_POST['submit'])){
+                $Name = $_POST['first_name']??"";
+                $TeacherId = $_POST['username']??"";
+                $EmailId = $_POST['email']??"";
+                $Password = $_POST['password']??"";
+              
+
+                $conn=mysqli_connect('localhost','root','','major');
+
+                $query="INSERT INTO `teacher`(`Teacher Id`, `Name`, `Email`, `Password`) VALUES ('$TeacherId','$Name','$EmailId','$Password')";
+                $run=mysqli_query($conn,$query);
+                if($run==True){
+              ?>      
+                   <script>
+                     alert("Your are Registered Successfully");
+                   </script>
+                   <?php
+                }
+      
+                  else{
+                      echo "Error: " .$run."<br>" .mysqli_error($conn);
+                  }
+              }
+             
+        ?>
+        
 	</div>
 </body>
 </html>
