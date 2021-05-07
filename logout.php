8d42da0d44aaa54eb6e3f0eca6bb3f0151200867
Pranavkant Nigam <nigamkpranav@gmail.com>
2021-05-07 16:15:15 +0530

    logout fixed

--- logout.php
+++ logout.php
@@ -2,6 +2,9 @@
     session_start();
     session_destroy();
     unset($_SESSION['user']);
+    unset($_SESSION['uname']);
+    unset($_SESSION['id']);
+    
     unset($_SESSION['password']);
     header("Location: index.php");
 ?>
